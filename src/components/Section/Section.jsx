fc7fc7731dceaabbbafd61dc1e0595dae41774f5
Daniil Plakhotnuk <dplakhotnuk@gmail.com>
2022-12-27 18:47:37 +0200

    shorten import

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,7 +1,5 @@
 import { StyledSection } from './Section.styled';
-import { Title } from 'components/Titles/Titles.styled';
-import { SecondaryTitle } from 'components/Titles/Titles.styled';
-
+import { Title, SecondaryTitle } from 'components/Titles/Titles.styled';
 export const Section = ({ title, children }) => {
   return (
     <StyledSection>
